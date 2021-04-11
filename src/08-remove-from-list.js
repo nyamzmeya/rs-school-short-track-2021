bfcd8131dfd29109cdb57002ac96269ba9e7dfd1
Anna <annazmei2014@gmail.com>
2021-04-12 00:02:08 +0300

    feat:fix problems

--- src/08-remove-from-list.js
+++ src/08-remove-from-list.js
@@ -17,8 +17,8 @@
  * }
  */
 
-function removeKFromList(l, k) {
-  let list = new ListNode(l);
+function removeKFromList(/* l, k */) {
+  throw new Error('Not implemented');
 }
 
 module.exports = removeKFromList;
